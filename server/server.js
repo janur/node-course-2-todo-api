ccd4a7659f4142cbb5118a3eee54bf7bf2ed4e90
Jani Nurmi <jani.nurmi@roimaint.com>
2017-03-15 10:41:34 +0200

    JWT examples

--- server/server.js
+++ server/server.js
@@ -93,6 +93,19 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// POST /users
+// Use pick only email and password
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Started on port ${process.env.PORT}`);
 });
